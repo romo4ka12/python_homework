200c378cbf4d73dab218cbbd2172821349204845
Roman <konoromada@mail.ru>
2026-03-25 12:34:58 +0500

    Practice 1

--- practice1.py
+++ practice1.py
@@ -1,1 +1,30 @@
-print("h")
+name = input("Enter customer name: ")
+
+item1 = input("Enter name of item 1: ")
+price1 = float(input("Enter price of item 1 (KZT): "))
+
+item2 = input("Enter name of item 2: ")
+price2 = float(input("Enter price of item 2 (KZT): "))
+
+people = int(input("Enter number of people: "))
+
+subtotal = price1 + price2
+tip = subtotal * 0.10
+total = subtotal + tip
+per_person = total / people
+
+print("=" * 30)
+print("CAFE BILL")
+print("=" * 30)
+print("Customer :", name)
+print(item1, ":", price1, "KZT")
+print(item2, ":", price2, "KZT")
+print("-" * 30)
+print("Subtotal :", subtotal, "KZT")
+print("Tip (10%) :", tip, "KZT")
+print("Total :", total, "KZT")
+print("Per person :", per_person, "KZT")
+print("=" * 30)
+
+print("Tip included:", tip > 0)
+print("Bill over 5000 KZT:", total > 5000)
